906c9fbd44cf4dfeb896d03d29c3395d88b79f19
Marcin Baszczewski <marcin@baszczewski.pl>
2022-10-21 12:53:13 +0200

    Minify CSS inside final package.

--- scripts/setup-package.js
+++ scripts/setup-package.js
@@ -3,6 +3,7 @@
  */
 const fs = require("fs");
 const path = require("path");
+const minify = require("csso").minify;
 
 const DIST_DIR = 'dist';
 
@@ -13,6 +14,25 @@ function updateVersion(version) {
 
         fs.writeFileSync(path.join(process.cwd(), DIST_DIR, fileName), source, "utf-8");
     }
+}
+
+function minifyStyle() {
+    const replaceRange = (s, start, end, substitute) => {
+        return s.substring(0, start) + substitute + s.substring(end);
+    }
+
+    const startStyle = `ELEMENT_STYLE = \``;
+    const endStyle = `\`;`;
+
+    let source = fs.readFileSync(path.join(process.cwd(), DIST_DIR, 'element.js')).toString('utf-8');
+
+    const startPos = source.indexOf(startStyle) + startStyle.length;
+    const endPos = source.indexOf(endStyle, startPos);
+    const style = minify(source.substring(startPos, endPos)).css;
+
+    source = replaceRange(source, startPos, endPos, style);
+
+    fs.writeFileSync(path.join(process.cwd(), DIST_DIR, 'element.js'), source, "utf-8");
 }
 
 function main() {
@@ -26,6 +46,7 @@ function main() {
     fs.writeFileSync(path.join(process.cwd(), DIST_DIR, 'package.json'), JSON.stringify(sourceObj, null, 2), "utf-8");
 
     updateVersion(sourceObj.version);
+    minifyStyle();
 }
 
 main();
